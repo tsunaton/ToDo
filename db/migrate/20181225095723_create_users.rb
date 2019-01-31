0cbd536a426157fd65b7a7c9aa247d964ab90478
lee sooryung <tsuna.tonight@gmail.com>
2019-01-31 12:37:49 +0900

    fix conflict

--- db/migrate/20181225095723_create_users.rb
+++ db/migrate/20181225095723_create_users.rb
@@ -3,10 +3,6 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :name
       t.string :email
-<<<<<<< HEAD
-=======
-      t.string :password
->>>>>>> origin/master
       t.string :password_digest
 
       t.timestamps
